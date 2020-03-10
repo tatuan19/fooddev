cebb845b5b5eae5ea6c35df8261a14588feeb605
tatuan19 <trananhtuan12a10@gmail.com>
2020-03-11 00:53:02 +0700

    Initial commit

---
+++ sql/data.sql
@@ -0,0 +1,43 @@
+﻿-- Product
+-- Pizza
+
+use WebCSDL
+
+select * from Product
+
+-- Pizza
+INSERT INTO Product
+VALUES 
+('PZ01',N'PIZZA BÒ THƯỢNG HẠNG','299000',N'Vừa Bò viên kiểu Ý, vừa thịt bò thượng hạng. Cắn một miếng thôi là cảm nhận được vị mềm ngọt!','pizza1','Pizza','6'),
+('PZ02',N'PIZZA HẢI SẢN KIỂU SINGAPORE','145000',N'Hải sản phải cay thì mới ngon. Chính là bữa tiệc kiểu Singapore đậm vị!!!','pizza2','Pizza','7'),
+('PZ03',N'PIZZA HẢI SẢN NHIỆT ĐỚI','250000',N'Thưởng thức ngay liều Pizzamin Sea để được "ngập ngụa" trong 3 tầng hương vị topping sảng khoái.','pizza3','Pizza','8'),
+('PZ04',N'1/2 PIZZA HẢI SẢN SỐT MAYONNAISE + 1/2 PIZZA GÀ PHÔ MAI','169000',N'Vừa có gà phô mai lại có thêm thịt heo muối mặn mặn ăn không biết ngán. Bàn tiệc đa dạng topping, sao không thử?!
+','pizza4','Pizza','0'),
+('PZ05',N'PIZZA GÀ SỐT TƯƠNG KIỂU NHẬT','230000',N'Là tín đồ pizza, sao không tận hưởng hương vị độc đáo & tinh tế, pha trộn giữa 2 nền ẩm thực gói gọn trong những miếng bánh ngay hôm nay!!
+!','pizza5','Pizza','15'),
+('PZ06',N'PIZZA XÚC XÍCH PEPPERONI','129000',N'Hương vị truyền thống của đất nước Ý, hương vị pizza ngất ngây đã qua bao thế hệ, nhưng vẫn không hề mất điểm trong lòng những fan cứng của pizza','pizza6','Pizza','0'),
+('PZ07',N'PIZZA BÒ & TÔM','155000',N'Vừa sang vừa ngon, vừa đã mắt vừa đã miệng, bữa tiệc Thịt Bò phối Hải Sản tươi - không thể tuyệt vời hơn!!','pizza7','Pizza','0'),
+('PZ08',N'PIZZA BÁNH MỲ','129000',N'Sự kết hợp độc đáo của biểu tượng phong cách Ý và biểu tượng ẩm thực Việt được yêu thích trên toàn thế giới','pizza8','Pizza','5'),
+('PZ09',N'PIZZA TÁO AN LÀNH','99000',N'Với những lát táo tươi trên nền sốt Caramel ngọt ngào và bột quế thơm nồng','pizza9','Pizza','7'),
+('PZ10',N'PIZZA HẢI SẢN PESTO XANH','169000',N'Là sự kết hợp hoàn hảo giữa tôm, cua, mực và bông cải xanh tươi ngon trên nền sốt Pesto Xanh đậm vị đặc trưng của phong cách ẩm thực miền bắc nước Ý','pizza10','Pizza','0')
+
+-- Hamburger
+INSERT INTO Product
+VALUES
+('HBG08',N'BURGER PHI LÊ GÀ ĐẶC BIỆT','76000',N'Nhiều lớp gà xen kẽ nhau kết hợp với sốt đặc trưng tạo nên hương vị không thể bỏ lỡ <3','hbg8','Hamburger','5'),
+('HBG09',N'BURGER PHÔ MAI','46000',N'Được tạo nên từ phô mai với công thức đặc biệt suốt hằng trăm năm của đất nước Pháp không thể bỏ lỡ!!!','hbg9','Hamburger','7'),
+('HBG10',N'BURGER SIÊU CAY','108000',N'Ớt tươi, mù tạt, tiêu, tỏi, gừng, vân vân và mây mây gia vị. Bạn có dám thử ???','hbg10','Hamburger','2')
+
+-- Milk tea
+INSERT INTO Product
+VALUES 
+('MT01',N'SỮA CARAMEL MACCHIATO','78000',N'Sữa caramel macchiato','milktea1','Milktea','25'),
+('MT02',N'SỮA MATCHA MACCHIATO','58000',N'Sữa matcha macchiato','milktea2','Milktea','33'),
+('MT03',N'SINH TỐ DƯA HẤU','46000',N'Sinh tố dưa hấu','milktea3','Milktea','10'),
+('MT04',N'NƯỚC CHANH SẢ MẬT ONG','42000',N'Nước chanh sả mật ong','milktea4','Milktea','5'),
+('MT05',N'XOÀI DỪA ĐÁ XAY','69000',N'Xoài Dừa Đá Xay','milktea5','Milktea','21'),
+('MT06',N'VIỆT QUẤT ĐÁ XAY ','59000',N'Việt Quất Đá Xay','milktea6','Milktea','45'),
+('MT07',N'CHANH TUYẾT','57000',N'Chanh Tuyết','milktea7','Milktea','5'),
+('MT08',N'TRÀ ĐÀO CAM SẢ','64000',N'Trà Đào Cam Sả','milktea8','Milktea','7'),
+('MT09',N'TRÀ TÁO BẠC HÀ','57000',N'Trà Táo Bạc Hà','milktea9','Milktea','29'),
+('MT10',N'TRÀ CHANH CHÉM GIÓ ','46000',N'Trà chanh chém gió','milktea10','Milktea','108')
---
+++ sql/query.sql
@@ -0,0 +1,29 @@
+﻿-- View
+SELECT * FROM [Customer]
+SELECT * FROM [Product]
+SELECT * FROM [Order]
+SELECT * FROM [OrderList]
+SELECT * FROM [Cart]
+
+-- Admin 
+UPDATE Customer
+SET Permission = '1111'
+WHERE Username = 'admin'
+
+DELETE FROM [Cart]
+DELETE FROM [Order]
+
+DELETE FROM [OrderList]
+WHERE CreateDa = '1578073318545'
+
+DELETE FROM [Product]
+WHERE ProductID = 'PZ100'
+
+-- Test
+UPDATE Product
+SET ProductID = 'PZ10'
+WHERE ProductID = 'PZ010'
+
+INSERT INTO Customer
+VALUES ('trananhtuan12a10@gmail.com','$2a$10$KJSYeLMhz9q0f/NVhHHFeuvjIO7Uj5OR51ldiF9LVtjwYBSPEPOi.',N'Tran AnhTuan',N'ABC','0981524316','0001')
+
